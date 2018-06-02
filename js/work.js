3e4d608fa3a04367ff87ffaca652a57d5fa23edc
Unknown <hn85412404@gmail.com>
2018-06-02 10:06:28 +0800

    路徑+css

--- js/work.js
+++ js/work.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     $('.filtr-item figure').each(function() {
             const str = $(this).attr('id').substr(4, 4)
             $(this).find('.work_pic').attr('src', "images/works/" + str + ".png");
-            $(this).find('.work_frame').css({ backgroundImage:"url(/images/works/" + str + ".png)"})
-            
+            $(this).find('.work_frame').css({ backgroundImage: "url(images/works/" + str + ".png)" })
+
             // console.log("images/works/" + str + ".png")
         })
         /***** Magnific-popup *****/
